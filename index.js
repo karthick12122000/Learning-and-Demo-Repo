98dcab807fd3053d3a8060f8ef180923450207ad
karthick12122000 <karthicknammakkal@gmail.com>
2023-10-14 19:11:00 +0530

    My Web App

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var app = express();
 app.get("/", (req, res) => {
   res.send("hello");
 });
-const port = 3000;
-app.listen(port, () => {
-  console.log("server is working at http://localhost:" + port);
-});
+// const port = 3000;
+// app.listen(port, () => {
+//   console.log("server is working at http://localhost:" + port);
+// });
+module.exports = app;
