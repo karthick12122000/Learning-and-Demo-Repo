99e03aab4fceb06611e164d248f9ad314580d9b7
karthick12122000 <karthicknammakkal@gmail.com>
2023-10-19 07:38:27 +0530

    Add Task

--- Public/script.js
+++ Public/script.js
@@ -1,6 +1,9 @@
 // Fetch and display ToDo items
 async function fetchAndDisplayTodos() {
-  const response = await fetch("http://localhost:4000/api/todos");
+  const response = await fetch("http://localhost:4000/api/todos", {
+    method: "GET",
+  });
+
   const todos = await response.json();
   console.log(todos);
   const todoList = document.getElementById("app");
@@ -18,4 +21,24 @@ async function fetchAndDisplayTodos() {
 }
 
 fetchAndDisplayTodos();
-console.log("listItem");
+var Add = document.getElementById("add");
+Add.addEventListener("click", async () => {
+  let value = document.querySelector("input").value;
+  try {
+    const response = await fetch("http://localhost:4000/api/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ task: value }),
+    });
+    if (!response.ok) {
+      throw new Error(`Request failed with status: ${response.status}`);
+    }
+    const data = await response.json(); // Note the use of response.json(), not response.JSON()
+    console.log(data);
+    fetchAndDisplayTodos();
+  } catch (err) {
+    console.error("Error:", err);
+  }
+});
